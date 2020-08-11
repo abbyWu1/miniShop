5bb2f4ad3debc621e4e52d6adfaf746e99d9ccc0
abbyw <abby.wu@unisco.com>
2020-08-11 17:29:11 +0800

    update structure

--- src/main/java/shop/register/controller/FirstController.java
+++ src/main/java/shop/register/controller/FirstController.java
@@ -1,4 +1,4 @@
-package my.cfk.springbootdemo.controller;
+package shop.register.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
