454506f6bfd39e180d7ff3852a3d9d004802f593
abbyw <abby.wu@unisco.com>
2020-08-12 18:34:10 +0800

    springBoot+swagger

--- src/main/java/shop/register/controller/RegisterController.java
+++ src/main/java/shop/register/controller/RegisterController.java
@@ -1,5 +1,6 @@
 package shop.register.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -7,9 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import shop.common.baseController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,21 +19,22 @@ import java.util.Map;
  * @Authror chenfk
  * @Date 2020-7-30 16:49
  */
+@Api(tags = "用户注册接口")
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/register")
 public class RegisterController {
 
-    @ApiOperation(value = "新增用户", notes = "根据用户实体创建用户")
+    @ApiOperation("根据用户实体创建用户")
     @ApiImplicitParam(name = "name", value = "用户名", required = true, dataType = "String")
     @PostMapping("/add")
     public @ResponseBody
-    Map<String, Object> addUser(@RequestBody String name) {
+    Map<String, Object> addUser(@RequestBody(required = true) String name) {
         Map<String, Object> map = new HashMap<>();
         map.put("result", "success");
         return map;
     }
 
-    @ApiOperation(value = "删除信息", notes = "根据Id删除指定对象")
+    @ApiOperation("根据Id删除指定对象")
     @ApiImplicitParam(name = "id", value = "id", required = true, dataType = "String", paramType = "path")
     @DeleteMapping("/{id}")
     public @ResponseBody
